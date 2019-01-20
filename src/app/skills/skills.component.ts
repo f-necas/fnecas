01a9c5bb9d1391409df774ed65414390cd037768
f-necas <necas.florian@gmail.com>
2019-01-20 23:51:56 +0100

    dev: Finish skills page

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-skills',
@@ -7,10 +6,43 @@ import { trigger, style, animate, transition } from '@angular/animations';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+  progSkills: Array<any>;
+  skills: any;
 
-  constructor() { }
+  constructor() {
+    this.progSkills = [
+      [{'c++': 'cplusplus'}, {'java': 'java'}, {'python': 'python'}],
+      [{'node.js': 'nodejs'}, {'angular': 'angularjs'}, {'wordpress': 'wordpress'}],
+      [{'html5': 'html5'}, {'css3': 'css3'}, {'scss': 'sass'} , {'javascript': 'javascript'}, {'typescript': 'typescript'}]
+    ];
+
+    this.skills = {'sk': {
+      'Management': [
+        'Founder of Phi Photographie Association',
+        'Team manager of Quality Service of LundiMatin',
+        'Freelancer in web development'
+      ],
+      'Electronic': [
+        'Arduino : creation of an hexapod robot',
+        'RPi & SPI interface for eink screen interaction'
+      ],
+      'Others': [
+        'Design : Photoshop, Lightroom, Premiere Pro',
+        'CAD: Solidworks'
+      ]
+    }};
+  }
 
   ngOnInit() {
   }
 
+  addClass(event): void {
+    event.target.className += ' colored';
+  }
+
+  removeClass(event): void {
+    event.target.className = event.target.className.replace(' colored', '');
+  }
+
 }
+
