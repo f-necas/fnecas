6a800f081ea17714ddeeb830a7c9c51c58595866
f-necas <necas.florian@gmail.com>
2019-01-31 10:01:41 +0100

    dev: add hobbies

---
+++ src/app/hobbies/hobbie.ts
@@ -0,0 +1,9 @@
+export class Hobbie {
+    name: string;
+    startDate: Date;
+}
+
+export class HobbiesGroup {
+    groupName: string;
+    hobbieGroup: Array<Hobbie>;
+}
--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hobbie, HobbiesGroup } from './hobbie';
 
 @Component({
   selector: 'app-hobbies',
@@ -7,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HobbiesComponent implements OnInit {
 
-  constructor() { }
+  hobbies: Array<HobbiesGroup>;
+
+  constructor() {
+    this.hobbies = [
+      { groupName: 'Sports',
+        hobbieGroup: [
+          {name: 'Rock Climbing', startDate: new Date(2018, 1)},
+          {name: 'Running / Trail', startDate: new Date(2017, 1)},
+          {name: 'Hiking', startDate: new Date(2017, 1)},
+          {name: 'Windsurf', startDate: new Date(2017, 1)},
+        ]
+      },
+      { groupName: 'Travel',
+        hobbieGroup: [
+          {name: 'USA / Canada', startDate: new Date(2012, 1)},
+          {name: 'Malaysia, Thailand, Vietnam, Sri Lanka', startDate: new Date(2012, 1)},
+          {name: 'Egypt', startDate: new Date(2012, 1)},
+          {name: 'Switzerland, Liechtenstein, Austria, Germany', startDate: new Date(2012, 1)},
+          {name: 'Spain, Italy, Scotland, UK', startDate: new Date(2012, 1)}
+        ]
+      },
+      { groupName: 'Misc',
+        hobbieGroup: [
+          {name: 'Photography', startDate: new Date(2012, 1)}
+        ]
+      }
+    ];
+  }
 
   ngOnInit() {
   }
